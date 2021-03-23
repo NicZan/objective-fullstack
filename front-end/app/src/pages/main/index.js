ca9fcebf84be5e1d1c16d8f84da1632d27ba554f
Nicolas <nicolas.alvarenga.3@gmail.com>
2021-03-22 22:42:13 -0300

    add base front end

---
+++ front-end/app/src/pages/main/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Main = (props) => {
+    return (
+        <div>             
+            <h1>Nada encontrado</h1>
+            <p>A página que você tentou acessar não existe.</p>
+        </div>
+    )
+}
+
+export default Main
